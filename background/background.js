6e913af8012171afc1ac86ea00f4b41743038a4e
PrashanthaTP <tpprashantha@gmail.com>
2023-09-22 21:50:23 +0530

    Feat: Get current tab info on keypress
    + Use tab api in bg script and use message passing to send it to content script

--- background/background.js
+++ background/background.js
@@ -1,7 +1,19 @@
 console.log("Hello From Background Script")
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-	let senderInfo = sender.tab ? "content script : " + sender.tab.url : "extension"
-	console.log( "(background script): Message Received : from <-" + senderInfo );
-	sendResponse({message:"response from background script : message received ",received:message})
+let tabList = []
+chrome.runtime.onMessage.addListener( async(message, sender, sendResponse) => {
+	//let senderInfo = sender.tab ? "content script : " + sender.tab.url : "extension"
+	console.log(message)
+	switch (message.type) {
+		case "GET_ACTIVETAB":
+			const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+			tabList.push(tab)
+			sendResponse({ tab })
+			//sendResponse({tab:"hi"})
+			return true
+		default:
+			break
+	}
+	//sendResponse({ message: "response from background script : message received ", received: message })
 });
+
--- content_scripts/content.js
+++ content_scripts/content.js
@@ -1,10 +1,34 @@
 console.log(document.title + " : Hello From Content Script");
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-	let senderInfo = sender.tab ? "content script : " + sender.tab.url : "extension"
-	console.log( "(content script): Message Received : from <-" + senderInfo );
+    let senderInfo = sender.tab ? "content script : " + sender.tab.url : "extension"
+    console.log("(content script): Message Received : from <-" + senderInfo);
     sendResponse({
         message: "response from content script : message received ",
         received: message,
     });
 });
+
+const addTab = async ()=>{
+    //const [tab] = await chrome.tabs.query({active:true,currentWindow:true})
+    //tabs api not available in content script
+    const response = await chrome.runtime.sendMessage({'type': 'GET_ACTIVETAB'})
+    console.log(response.tab)
+
+}
+let isValidPress = false
+document.addEventListener('keydown', (e) => {
+    console.log(e.key)
+    if(e.key === 'a' && isValidPress){
+        console.log("A pressed")
+        addTab()
+        isValidPress = false
+    }
+    if (e.shiftKey && e.key === 'E') {
+        isValidPress = true;
+        console.log("Shift + E")
+        setTimeout(()=>{
+            isValidPress = false
+        },1000)
+    }
+})
